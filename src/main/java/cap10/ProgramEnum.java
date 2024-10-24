a06e279ccde36a9a722ecd62da6bc0a6dae67428
Murilo Besson <murilo.besson@outlook.com>
2024-10-24 15:16:46 -0300

    Iniciando Enums

---
+++ src/main/java/cap10/ProgramEnum.java
@@ -0,0 +1,24 @@
+package cap10;
+
+import entities.Order;
+import entitiesenums.OrderStatus;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class ProgramEnum {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+
+        System.out.println(order);
+
+        OrderStatus os1 = OrderStatus.DELIVERED;
+        OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+        System.out.println(os1);
+        System.out.println(os2);
+        sc.close();
+    }
+}
---
+++ src/main/java/entitiesenums/OrderStatus.java
@@ -0,0 +1,8 @@
+package entitiesenums;
+
+public enum OrderStatus {
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED;
+}
