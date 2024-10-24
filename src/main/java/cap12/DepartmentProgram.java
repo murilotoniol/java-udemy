2eb48822502101ae67a472ea2157d7840c9d9c29
Murilo Besson <murilo.besson@outlook.com>
2024-10-24 17:16:24 -0300

    zzz

--- src/main/java/cap12/DepartmentProgram.java
+++ src/main/java/cap12/DepartmentProgram.java
@@ -54,8 +54,6 @@ public class DepartmentProgram {
         System.out.println("Department: " + worker.getDepartment().getName());
         System.out.println("Income for " + monthAndYear + ": " + String.format("%.2f", worker.income(year, month)));
 
-
-
         sc.close();
     }
 }
